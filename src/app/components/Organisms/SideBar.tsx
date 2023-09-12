69fc39e85296beaafdf5e6a6e2a59eadd6b59c9b
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 09:03:31 +0900

    theme.tsの作成

--- src/app/components/Organisms/SideBar.tsx
+++ src/app/components/Organisms/SideBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SidebarOption from '../Molecules/SidebarOption';
+import css from 'styled-jsx/css'
 
 import TwitterIcon from '@mui/icons-material/Twitter';
 import HomeIcon from '@mui/icons-material/Home';
@@ -12,19 +13,29 @@ import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { Button } from '@mui/material';
 
+const styles = css`
+
+`
+
 const SideBar = () => {
   return (
-    <div className='sidebar'>
-      <TwitterIcon className='sidebar-twitter-icon' />
-      <SidebarOption text='ホーム' Icon={HomeIcon} />
-      <SidebarOption text='話題を検索' Icon={SearchIcon} />
-      <SidebarOption text='通知' Icon={NotificationsNoneIcon} />
-      <SidebarOption text='メッセージ' Icon={MailOutlineIcon} />
-      <SidebarOption text='ブックマーク' Icon={BookmarkBorderIcon} />
-      <SidebarOption text='リスト' Icon={ListAltIcon} />
-      <SidebarOption text='プロフィール' Icon={PermIdentityIcon} />
-      <SidebarOption text='もっとみる' Icon={MoreHorizIcon} />
-    </div>
+    <>
+      <style jsx>{styles}</style>
+      <div className='sidebar'>
+        <TwitterIcon className='sidebar-twitter-icon' />
+        <SidebarOption text='ホーム' Icon={HomeIcon} />
+        <SidebarOption text='話題を検索' Icon={SearchIcon} />
+        <SidebarOption text='通知' Icon={NotificationsNoneIcon} />
+        <SidebarOption text='メッセージ' Icon={MailOutlineIcon} />
+        <SidebarOption text='ブックマーク' Icon={BookmarkBorderIcon} />
+        <SidebarOption text='リスト' Icon={ListAltIcon} />
+        <SidebarOption text='プロフィール' Icon={PermIdentityIcon} />
+        <SidebarOption text='もっとみる' Icon={MoreHorizIcon} />
+        <Button variant='outlined' className='sidebar-btn'>
+          ポストする
+        </Button>
+      </div>
+    </>
   );
 };
 
