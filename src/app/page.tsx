1b06f6ad22509c7723ebfb863464d58e90f64ea1
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 10:36:50 +0900

    Widgetsコンポーネントの実装、スタイル適用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SideBar from './components/Organisms/SideBar';
 import Timeline from './components/Organisms/Timeline';
+import Widgets from './components/Organisms/Widget';
 
 const App = () => {
   return (
     <div className='app'>
       <SideBar />
       <Timeline />
+      <Widgets />
     </div>
   );
 };
